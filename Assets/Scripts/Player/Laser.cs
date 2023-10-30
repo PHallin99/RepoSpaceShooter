e5d97866bd845cd43bfa8d761fc58b8468c6a3e5
PHallin <petter-hallin99@live.se>
2023-10-30 20:32:49 +0100

    Created laser script and a wrapping box for the screen

---
+++ Assets/Scripts/Player/Laser.cs
@@ -0,0 +1,26 @@
+﻿using UI;
+using UnityEngine;
+
+namespace Player {
+    public class Laser : MonoBehaviour {
+        [SerializeField] private float maxLifeTime;
+        private float moveSpeed;
+        private float lifeTimeSeconds;
+
+        private void Update() {
+            transform.Translate(Vector3.right * (ConstantsHandler.LaserMovementSpeed * Time.deltaTime));
+
+            if (lifeTimeSeconds >= maxLifeTime) {
+                Destroy(gameObject);
+                return;
+            }
+
+            lifeTimeSeconds += Time.deltaTime;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision) {
+            FindObjectOfType<UIUpdater>().AddScore(-5);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WrappingBox.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class WrappingBox : MonoBehaviour {
+    private bool isWrappingX;
+    private bool isWrappingY;
+
+    private void Update() {
+        ScreenWrap();
+    }
+
+    private void ScreenWrap() {
+        var isVisible = CheckRenderers();
+
+        if (isVisible) {
+            isWrappingX = false;
+            isWrappingY = false;
+            return;
+        }
+
+        if (isWrappingX && isWrappingY) {
+            return;
+        }
+
+        Vector2 newPosition = transform.position;
+
+        if (newPosition.x > 6.66f || newPosition.x < -6.66f) {
+            newPosition.x = -newPosition.x;
+            isWrappingX = true;
+        }
+
+        if (newPosition.y > -5 || newPosition.y < 5) {
+            newPosition.y = -newPosition.y;
+            isWrappingX = true;
+        }
+
+        transform.position = newPosition;
+    }
+
+    private bool CheckRenderers() {
+        var position = transform.position;
+        if (!(position.x < 6.66f) || !(position.x > -6.66f)) {
+            return false;
+        }
+
+        return position.y > -5 && position.y < 5;
+    }
+}
